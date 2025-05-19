64b70e5727dc74dca6ff7e71dda7308a8d98fe32
cyrilhussong <cyrilhussong347@gmail.com>
2025-05-19 20:58:35 +0200

    Page d'accueil changée vers Portfolio

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-// Import des composants Header et Footer
 import Header from './components/Header';
 import Footer from './components/Footer';
 
-// Import des pages
 import PageAccueil from './page/Page-accueil';
 import APropos from './page/A-propos';
 import Services from './page/Services';
@@ -14,14 +11,11 @@ import Realisations from './page/Realisation';
 import Blog from './page/Blog';
 import Contact from './page/Contact';
 import PageLegale from './page/Page-Legal';
-
-
-// Import du composant GitHubProfile
 import GitHubProfile from './components/GitHubProfile';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/optimiser-cv">
       <Header />
       <main>
         <Routes>
@@ -32,8 +26,6 @@ function App() {
           <Route path="/blog" element={<Blog />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/mentions-legales" element={<PageLegale />} />
-          
-          {/* Ajouter la route pour afficher le profil GitHub */}
           <Route path="/profil-github" element={<GitHubProfile />} />
         </Routes>
       </main>
