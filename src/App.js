38a6269b30d12c98756e9d5630bea17fbe28a48a
cyrilhussong <cyrilhussong347@gmail.com>
2025-05-19 21:15:48 +0200

    Ajout du basename dans Router pour GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-// Import des composants Header et Footer
 import Header from './components/Header';
 import Footer from './components/Footer';
 
-// Import des pages
 import PageAccueil from './page/Page-accueil';
 import APropos from './page/A-propos';
 import Services from './page/Services';
@@ -15,13 +12,11 @@ import Blog from './page/Blog';
 import Contact from './page/Contact';
 import PageLegale from './page/Page-Legal';
 
-
-// Import du composant GitHubProfile
 import GitHubProfile from './components/GitHubProfile';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/optimiser-cv">
       <Header />
       <main>
         <Routes>
@@ -32,8 +27,6 @@ function App() {
           <Route path="/blog" element={<Blog />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/mentions-legales" element={<PageLegale />} />
-          
-          {/* Ajouter la route pour afficher le profil GitHub */}
           <Route path="/profil-github" element={<GitHubProfile />} />
         </Routes>
       </main>
