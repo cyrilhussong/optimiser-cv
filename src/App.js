8d6498510a0b5169053a1a7a19731adcce0f7f61
cyrilhussong <cyrilhussong347@gmail.com>
2025-05-19 21:28:11 +0200

    Description courte de la modification

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-// Import des composants Header et Footer
+// Composants communs
 import Header from './components/Header';
 import Footer from './components/Footer';
 
-// Import des pages
+// Pages
 import PageAccueil from './page/Page-accueil';
 import APropos from './page/A-propos';
 import Services from './page/Services';
@@ -15,13 +15,12 @@ import Blog from './page/Blog';
 import Contact from './page/Contact';
 import PageLegale from './page/Page-Legal';
 
-
-// Import du composant GitHubProfile
+// Composant supplémentaire
 import GitHubProfile from './components/GitHubProfile';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/optimiser-cv"> {/* adapte ce basename si tu déploies sur GitHub Pages */}
       <Header />
       <main>
         <Routes>
@@ -32,8 +31,6 @@ function App() {
           <Route path="/blog" element={<Blog />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/mentions-legales" element={<PageLegale />} />
-          
-          {/* Ajouter la route pour afficher le profil GitHub */}
           <Route path="/profil-github" element={<GitHubProfile />} />
         </Routes>
       </main>
